bc48d472c41bf111fb06b00a7c04b690b68406cc
EthanNC <Ethan_Cumming@student.uml.edu>
2020-02-06 11:46:12 -0500

    Update product-page.js

--- src/templates/product-page.js
+++ src/templates/product-page.js
@@ -173,13 +173,6 @@ export const productPageQuery = graphql`
           author
           quote
         }
-        full_image {
-          childImageSharp {
-            fluid(maxWidth: 2048, quality: 100) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
       }
     }
   }
